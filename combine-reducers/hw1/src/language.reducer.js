16d761cff2f13b471806f973802ffeb608d13955
E-Martynenko1982 <motor8103@gmail.com>
2024-09-14 12:36:52 +0300

    update

--- combine-reducers/hw1/src/language.reducer.js
+++ combine-reducers/hw1/src/language.reducer.js
@@ -1,14 +1,14 @@
 import { LANGUAGE } from "./language.actions";
 
 const initialState = {
-  language: 'en'
+  language: null
 }
 
 export const languageReducer = (state = initialState, action) => {
   switch (action.type) {
     case LANGUAGE:
       return {
-        ...state,
+
         language: action.payload
       }
     default:
